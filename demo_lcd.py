3c306781e42936f76ef664109bc8dab19b945308
Sahaj Singh <sahaj444@gmail.com>
2021-04-02 11:02:57 -0400

    lcd

--- demo_lcd.py
+++ demo_lcd.py
@@ -6,7 +6,28 @@
 
 # Import necessary libraries for communication and display use
 import drivers
-from time import sleep
+from time import sleep, time
+import pyrebase
+
+
+from datetime import datetime
+
+now = datetime.now()
+current_time = now.strftime("%H:%M")
+print("Current Time =", current_time)
+
+config = {
+  "apiKey": "AIzaSyDFvJAre2oelCzHLjqFjviAf0hesiu-FcE",
+  "authDomain": "garageeye-e2848.firebaseapp.com",
+  "databaseURL": "https://garageeye-e2848-default-rtdb.firebaseio.com",
+  "storageBucket": "garageeye-e2848.appspot.com"
+}
+
+# initialisatiing pyrebase
+firebase = pyrebase.initialize_app(config)
+
+# initialisatiing Database
+db = firebase.database()
 
 # Load the driver and set it to "display"
 # If you use something from the driver library use the "display." prefix first
@@ -17,11 +38,13 @@ try:
     while True:
         # Remember that your sentences can only be 16 characters long!
         print("Writing to display")
-        display.lcd_display_string("Greetings Human!", 1)  # Write line of text to first line of display
-        display.lcd_display_string("Demo Pi Guy code", 2)  # Write line of text to second line of display
-        sleep(2)                                           # Give time for the message to be read
-        display.lcd_display_string("I am a display!", 1)   # Refresh the first line of display with a different message
-        sleep(2)                                           # Give time for the message to be read
+        GarageState = db.child("GarageState").get().val()
+        time = now.strftime("%H:%M")
+        display.lcd_display_string("Garage: " + GarageState, 1)  # Write line of text to first line of display
+        display.lcd_display_string(current_time, 2)  # Write line of text to second line of display
+        sleep(5)                                           # Give time for the message to be read
+        #display.lcd_display_string("I am a display!", 1)   # Refresh the first line of display with a different message
+        #sleep(2)                                           # Give time for the message to be read
         display.lcd_clear()                                # Clear the display of any data
         sleep(2)                                           # Give time for the message to be read
 except KeyboardInterrupt:
